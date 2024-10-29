2ece66a35855f9cacdac1aaa52e2c442bd935f8e
Grace Ayuni Sinta Sitorus <gracesitorus@photon.com>
2024-10-29 12:15:04 +0700

    “test”

--- Technical_Test_Paragon/cypress.config.js
+++ Technical_Test_Paragon/cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
+    baseUrl: 'https://recruitment-staging-queenbee.paradev.io/',
+    excludeSpecPattern: ['*.spec.cy.js'],
+    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
+
     },
   },
 });
--- Technical_Test_Paragon/cypress/e2e/spec.cy.js
+++ Technical_Test_Paragon/cypress/e2e/spec.cy.js
@@ -1,8 +1,40 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://recruitment-staging-queenbee.paradev.io/')
+/// <reference types="cypress" />
 
-    cy.contains('x').click()
-    cy.contains('Daftar Jadi Member').click()
-  })
-})
+describe('First Test Suite', () => {
+  it('Register', () => {
+    cy.visit('/');
+    cy.get('body').click(0, 0);
+    cy.get('#chakra-modal-1').should('not.exist');
+    cy.contains('Daftar Jadi Member').click();
+
+    cy.contains('[class="css-0"]', 'Apakah Anda memiliki Kode Referral dari Sponsor?').find('[type="radio"]').then( radioButtons =>{
+      cy.wrap(radioButtons).eq(1).check({force: true});
+    cy.contains('Lanjut').click();
+
+    cy.get('input[name="name"]').type('Grace Sitorus');
+    // cy.get('input[name="name"]').invoke('prop', 'value').should('contain', 'Grace Sitorus').then(property =>{
+    //   expect(property).to.equal('Grace Sitorus')
+    // })
+
+    cy.get('input[name="phone"]').type('81234567890');
+    // cy.get('input[name="phone"]').invoke('prop', 'value').should('contain', '81234567890').then(property =>{
+    //   expect(property).to.equal('81234567890')
+    // })
+
+    cy.get('input[name="email"]').type('grace1600ail.com');
+    // cy.get('input[name="email"]').invoke('prop', 'value').should('contain', 'grace1600ail.com').then(property =>{
+    //   expect(property).to.equal('grace1600ail.com')
+    // })
+
+    cy.get('input[name="password"]').type('Password@123');
+    // cy.get('input[name="password"]').invoke('prop', 'value').should('contain', 'Password@123').then(property =>{
+    //   expect(property).to.equal('Password@123')
+    // })
+
+    cy.wait(500); // Adjust the wait time as needed
+    cy.get('button[type="button"]').click();
+
+
+    });
+  });
+});
